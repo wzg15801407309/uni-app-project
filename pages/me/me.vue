5695ca0b004695bb8b586c46996b5ffa398a83dc
wzg15801407309 <1051084301@qq.com>
2020-11-05 14:22:29 +0800

    我的界面制作

--- pages/me/me.vue
+++ pages/me/me.vue
@@ -1,6 +1,30 @@
 <template>
-	<view>
-		这是个人页面
+	<view class="page page-fill"> 
+		<view class="header">
+			<view v-if="userIsLogin">
+				<image src="../../static/nazha.jpeg" class="face"></image>
+			</view>
+			<view v-else>
+				<image src="../../static/default-header.png" class="face"></image>
+			</view>
+			<view class="info-wapper" v-if="userIsLogin">
+				<view class="nickname">
+					NEXT 学院
+				</view>
+				<view class="nav-info">id:25105 29233 20320</view>
+			</view>
+			<view v-else>
+				<navigator url="../registLogin/registLogin">
+					<view class="nickname regist-login">
+						注册/登陆
+					</view>
+				</navigator>
+
+			</view>
+			<view v-if="userIsLogin" class="set-wapper">
+				<image src="../../static/setting.png" class="settings"></image>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +32,7 @@
 	export default {
 		data() {
 			return {
-				
+				userIsLogin:false
 			}
 		},
 		methods: {
@@ -18,5 +42,5 @@
 </script>
 
 <style>
-
+@import url("me.css");
 </style>
---
+++ pages/registLogin/registLogin.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="body">
+		<view class="face-wapper">
+			<image src="../../static/default-header.png" class="face"></image>
+		</view>
+		<view class="info-wapper">
+			<label class="words-lbl">账号</label>
+			<input name='username' type="text" value="" class="input" placeholder="请输入用户名" placeholder-class="graywords"></input>
+		</view>
+		<view class="info-wapper" style="margin-top: 40upx;">
+			<label class="words-lbl">密码</label>
+			<input name='password' type="text" password="true" value="" class="input" placeholder="请输入密码" placeholder-class="graywords"></input>
+		</view>
+		<button type="primary" style="margin-top: 60upx; width: 90%;">注册/登陆</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+@import url("registLogin.css");
+</style>
