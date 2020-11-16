528409add8e8d7024f2791955b15c8557c1a1955
wzg15801407309 <1051084301@qq.com>
2020-11-16 17:56:14 +0800

    创建相册上传界面

---
+++ pages/meFace/meFace.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="page page-fill">
+		<view class="pending-wapper">
+			<image src="../../static/swper/swiper-1.jpeg" class="pending-face" mode="scaleToFill"></image>
+		</view>
+		<view class="notice">
+			<view class="notice-words">
+				*请从相册中选择等比宽高的图片噢～
+			</view>
+		</view>
+		<view class="footer-opertor">
+			<view class="opertor-words" @click="">
+				重新选择
+			</view>
+			<view class="opertor-words" @click="">
+				确认上传
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	@import url("meFace.css");
+</style>
